157847145774cb8048491e61e6868f3442667a7e
231009 <231009@CPD03-M07>
2025-02-17 11:41:53 -0300

    Tiagotas do Val, meu gnominho da liberdade.

--- MauiComprinhasDaShein/Views/ListaProduto.xaml.cs
+++ MauiComprinhasDaShein/Views/ListaProduto.xaml.cs
@@ -6,4 +6,34 @@ public partial class ListaProduto : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void ToolbarItem_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void ToolbarItem_Clicked_1(object sender, EventArgs e)
+    {
+
+    }
+
+    private void txt_search_TextChanged(object sender, TextChangedEventArgs e)
+    {
+
+    }
+
+    private void lst_produtos_Refreshing(object sender, EventArgs e)
+    {
+
+    }
+
+    private void lst_produtos_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+    {
+
+    }
+
+    private void MenuItem_Clicked(object sender, EventArgs e)
+    {
+
+    }
 }
