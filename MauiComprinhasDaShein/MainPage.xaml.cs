974ce75913b5b4555a1712b06649f31eb339aaff
Heitor O. Botelho <heitorobotelho@gmail.com>
2025-03-12 17:43:19 -0300

    Agora o código funciona.

--- MauiComprinhasDaShein/MainPage.xaml.cs
+++ MauiComprinhasDaShein/MainPage.xaml.cs
@@ -1,10 +1,24 @@
-﻿namespace MauiComprinhasDaShein
+﻿using MauiComprinhasDaShein.Views;
+
+namespace MauiComprinhasDaShein
 {
     public partial class MainPage : ContentPage
     {
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private void ToolbarItem_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                Navigation.PushAsync(new ListaProduto());
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("OPS!!", ex.Message, "OK");
+            }
         }
     }
 
